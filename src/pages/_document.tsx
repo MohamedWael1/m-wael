2f04a45ea7c5e6ff9916d7dc6f1e73d93b439bc8
MohamedWael1 <mohwael00@gmail.com>
2023-07-27 17:24:58 +0200

    update og image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ export default function Document() {
 
                 <meta property="og:title" content="Mohamed Wael" />
                 <meta property="og:description" content="Explore Mohamed Wael's impressive software developer portfolio showcasing projects, skills, and expertise in web development." />
-                <meta property="og:image" content="/imgs/personal-pic.jpg" />
+                <meta property="og:image" content="/imgs/website-screenshot.png" />
                 <meta property="og:url" content="https://m-wael.vercel.app/" />
                 <meta property="og:site_name" content="Mohamed Wael" />
                 <meta name="twitter:card" content="🚀 Welcome! I'm Mohamed, a passionate full-stack web developer with expertise in JavaScript, Typescript, React, and Node.js. Explore my projects and witness the power of intuitive, user-friendly applications. Let's build something amazing together! #webdev #portfolio" />
