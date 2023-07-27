dc3ebdedfcf2e3a806f6acdce505b9b4641b5750
MohamedWael1 <mohwael00@gmail.com>
2023-07-27 03:54:49 +0200

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,24 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <title>Mohamed Wael</title>
+                <meta name="author" content="Mohamed Wael" />
+                <meta
+                    name="description"
+                    content="Explore Mohamed Wael's impressive software developer portfolio showcasing projects, skills, and expertise in web development."
+                />
+                <meta
+                    name="keywords"
+                    content="software developer, web development, programming, projects, portfolio"
+                />
+
+                <meta property="og:title" content="Mohamed Wael" />
+                <meta property="og:description" content="Explore Mohamed Wael's impressive software developer portfolio showcasing projects, skills, and expertise in web development." />
+                <meta property="og:image" content="/imgs/personal-pic.jpg" />
+                <meta property="og:url" content="https://m-wael.vercel.app/" />
+                <meta property="og:site_name" content="Mohamed Wael" />
+                <meta name="twitter:card" content="🚀 Welcome! I'm Mohamed, a passionate full-stack web developer with expertise in JavaScript, Typescript, React, and Node.js. Explore my projects and witness the power of intuitive, user-friendly applications. Let's build something amazing together! #webdev #portfolio" />
+                <meta name="twitter:image:alt" content="Portrait of  Mohamed Wael, software developer" />
                 <link
                     href="https://db.onlinewebfonts.com/c/4c19fc875e7ba1e6831129de3ab5ac0b?family=Retro+Gaming"
                     rel="stylesheet"
